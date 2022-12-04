ea0c56df827709705505a6d6543311ef4040e8fa
Tanner Kothlow <tannerkothlow@msn.com>
2022-12-04 10:16:10 -0600

    GET route for single post shows comments

--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -5,6 +5,35 @@ const { User, Post, Comment } = require('../models');
 // GET: gets post by ID and brings its comments along
 // POST: adds a comment
 
+router.get('/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['body', 'date_created'],
+                    include: [
+                        {
+                            model: User,
+                            attributes: ['username'],
+                        },
+                    ],
+                },
+            ],
+        });
+        const post = postData.get({ plain: true });
+        
+        console.log(post);
+        res.render('post', {post})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 // /
 // GET: returns the login page
 // POST: adds your comment
