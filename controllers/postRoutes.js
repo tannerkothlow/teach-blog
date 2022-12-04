d36e958d1670f888e8a78a86a2cf245fe03c6b1a
Tanner Kothlow <tannerkothlow@msn.com>
2022-12-04 11:29:12 -0600

    Post by ID comment

--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -60,4 +60,17 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.post('/:id', async (req, res) => {
+    try {
+        const commentData = await Comment.create({
+            body: req.body.body,
+            user_id: 1,
+            post_id: Number(req.params.id),
+        });
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
