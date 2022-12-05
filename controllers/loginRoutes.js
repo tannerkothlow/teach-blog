926adf51536ad10ea14ef326e2a47878079c37b5
Tanner Kothlow <tannerkothlow@msn.com>
2022-12-05 09:16:57 -0600

    Login route get page and post login

--- controllers/loginRoutes.js
+++ controllers/loginRoutes.js
@@ -3,6 +3,48 @@ const { User } = require('../models');
 
 // /
 // GET: gets login page
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('login')
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 // POST: logs in
+
+router.post('/', async (req, res) => {
+    try {
+        // req.body.email
+        // req.body.password
+        const userData = await User.findOne({ where: { email: req.body.email } });
+
+        if (!userData) {
+            res.status(400).json({ 
+                message: 'Incorrect email or password, please try again' 
+            });
+            return;
+        }
+        // Passes req password into a helper function that compares it to the stored password.
+        const validPassword = userData.checkPassword(req.body.password)
+
+        if (!validPassword) {
+            res.status(400).json({ 
+                message: 'Incorrect email or password, please try again' 
+            });
+            return;
+        }
+
+        req.session.save(() => {
+            // Will save what user is logged in.
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.status(200).json({ message: 'You are now logged in!' })
+        });
+    } catch (err) {
+
+    }
+})
 
 module.exports = router;
